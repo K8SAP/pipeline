1e892afa39ea3de1e5b4d3e4875897947d2aa5a7
I570399 <andrew.gugger@sap.com>
2025-01-16 21:56:40 -0500

    testing

--- calculator.py
+++ calculator.py
@@ -11,9 +11,9 @@ if __name__ == "__main__":
 
 # Tests
 def test_add():
-    assert add(2, 3) == 5
+    assert add(3, 3) == 5
     assert add(-1, 1) == 0
 
 def test_subtract():
     assert subtract(5, 3) == 2
-    assert subtract(0, 3) == -3
+    assert subtract(0, 2) == -3
